7103df6c6ff7fd22b4270c4da65d523fbedc7449
giannhsker <giannhsker3@gmail.com>
2025-09-20 14:55:20 +0300

    first commit

---
+++ src/main/java/com/gi/pharmaceuticalStorage/PharmaceuticalStorageApplication.java
@@ -0,0 +1,13 @@
+package com.gi.pharmaceuticalStorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaceuticalStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaceuticalStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gi/pharmaceuticalStorage/PharmaceuticalStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gi.pharmaceuticalStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaceuticalStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
